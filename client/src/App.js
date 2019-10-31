237f75a2c3894a4eac5e22b72809d297e70985e9
anel mesic <anel.mesic@appliedtechnology.se>
2019-10-31 10:19:02 +0100

    Added search function in React to search on teams

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ import './App.css'
 
 function App() {
   const [state, setstate] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('Hammarby')
+
 
   useEffect(() => {
     getFootballStats()
-  }, [])
+  }, [query])
 
   const getFootballStats = async () => {
     const res = await fetch(`http://localhost:5000/allsvenskan/${query}`)
@@ -17,9 +20,30 @@ function App() {
     setstate(data)
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <form onClick={getSearch} className="search-form">
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
         {state.map(stats => (
           <Schedule
             home={stats.homeName}
